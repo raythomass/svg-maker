034085d1ec9092c1a72017a20f03b632ac86acdd
Ray Thomas <rthomas5678@gmail.com>
2023-11-01 16:04:22 -0700

    added files and initial usability

---
+++ index.js
@@ -0,0 +1,54 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const shapes = require('./lib/shapes');
+
+const questions = [
+    {
+        type: 'input',
+        name: 'text',
+        message: 'Enter up to three characetrs to be displayed on the logo.'
+    },
+    {
+        type: 'input',
+        name: 'textcolor',
+        message: 'Enter a color name or a hexidecimal number to choose the text color.'
+    },
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'Choose a shape for your logo.',
+        choices: [
+            'Circle',
+            'Triangle',
+            'Square',
+        ]
+    },
+    {
+        type: 'input',
+        name: 'shapecolor',
+        message: 'Enter a color name or a hexidecimal number to choose the shape color.'
+    },
+];
+
+function makeShape(){
+    if(response.shape === 'circle'){
+        let newShape = new Circle(response.text, response.textcolor, response.shapecolor);
+        return newShape.render();
+    }
+    if(response.shape === 'triangle'){
+        let newShape = new Triangle(response.text, response.textcolor, response.shapecolor);
+        return newShape.render();
+    }
+    if(response.shape === 'square'){
+        let newShape = new Circle(response.text, response.textcolor, response.shapecolor);
+        return newShape.render();
+    };
+    
+}
+
+function init() {
+    inquirer.prompt(questions)
+};
+
+init();
+
