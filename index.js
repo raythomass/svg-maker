759e81f93192a0991961a37a2c9aea81fa74e04f
Ray Thomas <rthomas5678@gmail.com>
2023-11-05 14:16:31 -0700

    added example logo made from cli

--- index.js
+++ index.js
@@ -1,34 +1,86 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const shapes = require('./lib/shapes');
+const {Circle, Triangle, Square} = require('./lib/shapes.js');
 
-const questions = [
-    {
-        type: 'input',
-        name: 'text',
-        message: 'Enter up to three characetrs to be displayed on the logo.'
-    },
-    {
-        type: 'input',
-        name: 'textcolor',
-        message: 'Enter a color name or a hexidecimal number to choose the text color.'
-    },
-    {
-        type: 'list',
-        name: 'shape',
-        message: 'Choose a shape for your logo.',
-        choices: [
-            'Circle',
-            'Triangle',
-            'Square',
-        ]
-    },
-    {
-        type: 'input',
-        name: 'shapecolor',
-        message: 'Enter a color name or a hexidecimal number to choose the shape color.'
-    },
-];
+function run(){
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'text',
+            message: 'Enter up to three characetrs to be displayed on the logo.'
+        },
+        {
+            type: 'input',
+            name: 'textcolor',
+            message: 'Enter a color name or a hexidecimal number to choose the text color.'
+        },
+        {
+            type: 'list',
+            name: 'shape',
+            message: 'Choose a shape for your logo.',
+            choices: [
+                'Circle',
+                'Triangle',
+                'Square',
+            ]
+        },
+        {
+            type: 'input',
+            name: 'shapecolor',
+            message: 'Enter a color name or a hexidecimal number to choose the shape color.'
+        },
+    ])
+    .then((response) => {
+        if(response.shape === 'Circle'){
+            let newShape = new Circle(response.text, response.textcolor, response.shapecolor);
+            // newShape.render();
+            return fs.writeFile('logo.svg', newShape.render(), (err) =>
+            err ? console.log(err) : console.log('Successful'))
+        }
+        else if(response.shape === 'Triangle'){
+            let newShape = new Triangle(response.text, response.textcolor, response.shapecolor);
+            // return newShape.render();
+            return fs.writeFile('logo.svg', newShape.render(), (err) =>
+            err ? console.log(err) : console.log('Successful'))
+        }
+        else if(response.shape === 'Square'){
+            let newShape = new Square(response.text, response.textcolor, response.shapecolor);
+            // return newShape.render();
+            return fs.writeFile('logo.svg', newShape.render(), (err) =>
+            err ? console.log(err) : console.log('Successful')
+          );
+        }
+        
+    })
+}
+
+// const questions = [
+//     {
+//         type: 'input',
+//         name: 'text',
+//         message: 'Enter up to three characetrs to be displayed on the logo.'
+//     },
+//     {
+//         type: 'input',
+//         name: 'textcolor',
+//         message: 'Enter a color name or a hexidecimal number to choose the text color.'
+//     },
+//     {
+//         type: 'list',
+//         name: 'shape',
+//         message: 'Choose a shape for your logo.',
+//         choices: [
+//             'Circle',
+//             'Triangle',
+//             'Square',
+//         ]
+//     },
+//     {
+//         type: 'input',
+//         name: 'shapecolor',
+//         message: 'Enter a color name or a hexidecimal number to choose the shape color.'
+//     },
+// ];
 
 function makeShape(){
     if(response.shape === 'circle'){
@@ -43,20 +95,13 @@ function makeShape(){
         let newShape = new Circle(response.text, response.textcolor, response.shapecolor);
         return newShape.render();
     }
-    return fs.writeFile('logo.svg',newShape.render());
-}
+};
 
 function makeLogo(response){
-    let svgshape = makeShape(response);
-    fs.writeFile('logo.svg', svgshape);
+    // let svgshape = makeShape(response);
+    fs.writeFile('logo.svg', newShape.render());
 };
 
-function init() {
-    inquirer.prompt(questions)
-    .then((response) => {
-        makeLogo(response);
-    });
-};
 
-init();
+run();
 
